3b4e2b2dca4c1a5facfc5b5e1f034eafcf3e5cc0
Helena Utzig <helenautzig@gmail.com>
2025-07-07 15:05:57 +0300

    Board component rendering Square component with data passed through props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
-export default function Square() {
-  return <button className="square"> 🍑 </button>;
+function Square({ value }) { 
+  // use props to pass the value each square should have 
+  // from the parent component (Board) to its child (Square).
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+//  return <button className="square">🍑</button>;
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value=""/>
+        <Square value=""/>
+        <Square value=""/>
+      </div>
+      <div className="board-row">
+        <button className="square">🍑</button>
+        <button className="square">🍑</button>
+        <button className="square">🍑</button>
+      </div>
+    </>
+  );
 }
