7caaecdc4c9a345546736fa637a2c0d2e6c738dd
Helena Utzig <helenautzig@gmail.com>
2025-07-08 18:44:03 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -81,10 +81,21 @@ function getWinner(squares) {
 }
 
 function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+  
   return (
     <div className="game">
       <div className="game-board">
         <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
       </div>
     </div>
   );
